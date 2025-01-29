01d4936befc2ba3175cbfb2e0097bad4bd80f24c
T0madon <joao.daciuk@gmail.com>
2025-01-29 16:36:05 -0300

    label

--- main.py
+++ main.py
@@ -5,15 +5,19 @@ from PySide6.QtGui import QIcon
 from info import Info
 from variables import WINDOW_ICON_PATH
 from components.display import Display
+from styles import setupTheme
+from buttons import ButtonsGrid
 
 if __name__ == '__main__':
     # Cria aplicação
     app = QApplication(sys.argv)
+    setupTheme(app)
     window = MainWindow()
     
     # Define o ícone da aplicação
     icon = QIcon(str(WINDOW_ICON_PATH))
     window.setWindowIcon(icon)
+    app.setWindowIcon(icon)
 
     # Info 
     info = Info('2.0 ^ 10.0 = 1024')
@@ -21,8 +25,11 @@ if __name__ == '__main__':
 
     # Display
     display = Display()
-    
     window.addToVLayout(display)
+
+    # Grid
+    buttonsGrid = ButtonsGrid(display, info, window)
+    window.vLayout.addLayout(buttonsGrid)
 
     # Executa tudo
     window.adjustFixedSize()
