1257654bb19b6653c044ebfcaebd73068f7a541f
T0madon <joao.daciuk@gmail.com>
2025-02-01 14:57:04 -0300

    get-err-msg

--- buttons.py
+++ buttons.py
@@ -8,6 +8,7 @@ from utils import isEmpty, isNumOrDot, isValidNumber
 
 if TYPE_CHECKING:
     from components.display import Display
+    from main_window import MainWindow
     from info import Info
 
 class Button(QPushButton):
@@ -23,7 +24,10 @@ class Button(QPushButton):
         self.setMinimumSize(75, 75)
 
 class ButtonsGrid(QGridLayout):
-    def __init__(self, display: 'Display', info: 'Info', *args, **kwargs) -> None:
+    def __init__(
+            self, display: 'Display', info: 'Info', window: 'MainWindow',
+            *args, **kwargs
+    ) -> None:
         super().__init__(*args, **kwargs)
         
         self._gridMask = [
@@ -35,6 +39,7 @@ class ButtonsGrid(QGridLayout):
         ]
         self.display = display
         self.info = info
+        self.window = window
         self._equation = ''
         self._equationInitialValue = 'Sua conta'
         self._left = None
@@ -115,7 +120,7 @@ class ButtonsGrid(QGridLayout):
 
         # Se a pessoa clicou no operador sem configurar qualquer número
         if not isValidNumber(displayText) and self._left is None:
-            print('Não tem nada para colocar no valor da esquerda')
+            self._showError('Você não digitou nada.')
             return
         
         # Se houver algo no número da esquerda,não fazemos nada. Aguardaremos o número da direita.
@@ -129,7 +134,7 @@ class ButtonsGrid(QGridLayout):
         displayText = self.display.text()
 
         if not isValidNumber(displayText):
-            print('Vazio')
+            self._showError('Conta incompleta')
             return
         
         self._right = float(displayText)
@@ -142,9 +147,9 @@ class ButtonsGrid(QGridLayout):
             else:
                 result = eval(self.equation)
         except ZeroDivisionError:
-            print('divisão por zero')
+            self._showError('Você está dividindo por zero')
         except OverflowError:
-            print('Número muito grande')
+            self._showError('Conta não realizada')
 
         self.display.clear()
         self.info.setText(f'{self.equation} = {result}')
@@ -153,3 +158,18 @@ class ButtonsGrid(QGridLayout):
 
         if result == 'error':
             self._left = None
+
+    def _makeDialog(self, text):
+        msgBox = self.window.makeMsgBox()
+        msgBox.setText(text)
+        return msgBox
+    
+    def _showError(self, text):
+        msgBox = self._makeDialog(text)
+        msgBox.setIcon(msgBox.Icon.Critical)
+        msgBox.exec()
+
+    def _showInfo(self, text):
+        msgBox = self._makeDialog(text)
+        msgBox.setIcon(msgBox.Icon.Information)
+        msgBox.exec()
